4851265de9aae69d9e38bcb8be0ca55096b51fb8
byd_tw <1214658495@qq.com>
2019-01-16 11:26:09 +0800

    add feat1 on feat1

--- app/src/main/java/com/bydauto/appupdate/MyApplication.java
+++ app/src/main/java/com/bydauto/appupdate/MyApplication.java
@@ -1,13 +1,19 @@
 package com.bydauto.appupdate;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.tencent.bugly.Bugly;
 
 public class MyApplication extends Application {
+    private static final String TAG = "MyApplication";
     @Override
     public void onCreate() {
         super.onCreate();
         Bugly.init(getApplicationContext(), "0c07b3a819", false);
+    }
+
+    public void feat1() {
+        Log.e(TAG, "feat1: add function feat1 on feat1");
     }
 }
